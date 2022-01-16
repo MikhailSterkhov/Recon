e63f74e19ca38ed970d37f513eea461566d76d9e
itzstonlex <seltixfromhell@bk.ru>
2022-01-16 14:50:36 +0400

    `SocketFactory`: Made initialization through the opening of `SocketChannel`'s

--- src/main/java/org/itzstonlex/recon/factory/SocketFactory.java
+++ src/main/java/org/itzstonlex/recon/factory/SocketFactory.java
@@ -1,54 +1,63 @@
 package org.itzstonlex.recon.factory;
 
-import org.itzstonlex.recon.error.ReconRuntimeError;
+import org.itzstonlex.recon.exception.ReconRuntimeException;
 import org.itzstonlex.recon.option.ChannelOption;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
 
 public final class SocketFactory {
 
     public static ServerSocket createServerSocket(ChannelOption[] options, InetSocketAddress address) {
         try {
-            ServerSocket serverSocket = new ServerSocket();
+            ServerSocketChannel channel = ServerSocketChannel.open();
 
             for (ChannelOption channelOption : options) {
-                channelOption.apply(serverSocket);
+                try {
+                    channelOption.apply(channel);
+                }
+                catch (UnsupportedOperationException ignored) {
+                    // ignored exception.
+                }
             }
 
             InetSocketAddress resolvedAddress = new InetSocketAddress(address.getHostString(), address.getPort());
-            serverSocket.bind(resolvedAddress);
+            channel.bind(resolvedAddress);
 
-            return serverSocket;
+            return channel.socket();
         }
         catch (IOException exception) {
-            throw new ReconRuntimeError(exception);
+            throw new ReconRuntimeException(exception);
         }
     }
 
     public static Socket createClientSocket(ChannelOption[] options, InetSocketAddress address, int timeout) {
         try {
-            Socket socket = new Socket();
-            socket.setSoTimeout(timeout);
+            SocketChannel channel = SocketChannel.open();
 
             for (ChannelOption channelOption : options) {
-                channelOption.apply(socket);
+                channelOption.apply(channel);
             }
+
+            Socket socket = channel.socket();
+            socket.setSoTimeout(timeout);
 
             try {
                 InetSocketAddress resolvedAddress = new InetSocketAddress(address.getHostString(), address.getPort());
                 socket.connect(resolvedAddress, timeout);
             }
             catch (IOException ignored) {
+                // ignored exception.
             }
 
             return socket;
         }
         catch (IOException exception) {
-            throw new ReconRuntimeError(exception);
+            throw new ReconRuntimeException(exception);
         }
     }
-
 }
