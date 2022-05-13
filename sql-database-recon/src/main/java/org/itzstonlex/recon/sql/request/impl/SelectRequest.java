57fc5e197ac5a94cc54b0619f664c8111a74a977
itzstonlex <seltixfromhell@bk.ru>
2022-05-13 07:35:28 +0400

    `sql-database`: add LIKE, > and < signs for SELECT

--- sql-database-recon/src/main/java/org/itzstonlex/recon/sql/request/impl/SelectRequest.java
+++ sql-database-recon/src/main/java/org/itzstonlex/recon/sql/request/impl/SelectRequest.java
@@ -4,7 +4,9 @@ import org.itzstonlex.recon.sql.request.ReconSqlRequest;
 import org.itzstonlex.recon.sql.request.field.impl.ValuedField;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public final class SelectRequest extends ReconSqlRequest<ValuedField> {
@@ -13,6 +15,10 @@ public final class SelectRequest extends ReconSqlRequest<ValuedField> {
 
     private int limit = -1;
     private String[] selectedRows = {"*"};
+
+    private final Set<String> likeThenSet = new HashSet<>();
+    private final Set<String> moreThenSet = new HashSet<>();
+    private final Set<String> lessThenSet = new HashSet<>();
 
     public SelectRequest(String table) {
         this.table = table;
@@ -25,6 +31,27 @@ public final class SelectRequest extends ReconSqlRequest<ValuedField> {
 
     public SelectRequest limit(int limit) {
         this.limit = limit;
+        return this;
+    }
+
+    public SelectRequest pushLikeThen(ValuedField field) {
+        likeThenSet.add(field.name());
+
+        super.push(field);
+        return this;
+    }
+
+    public SelectRequest pushMoreThen(ValuedField field) {
+        moreThenSet.add(field.name());
+
+        super.push(field);
+        return this;
+    }
+
+    public SelectRequest pushLessThen(ValuedField field) {
+        lessThenSet.add(field.name());
+
+        super.push(field);
         return this;
     }
 
@@ -49,7 +76,22 @@ public final class SelectRequest extends ReconSqlRequest<ValuedField> {
         if (!fieldsList.isEmpty()) {
 
             requestBuilder.append(" WHERE ");
-            requestBuilder.append(String.join(" AND ", fieldsList.stream().map(field -> "`" + field.name() + "`=?").collect(Collectors.toSet())));
+            requestBuilder.append(String.join(" AND ", fieldsList.stream().map(field -> {
+
+                String sign = ("=");
+
+                if (likeThenSet.contains(field.name())) {
+                    sign = (" LIKE ");
+                }
+                else if (moreThenSet.contains(field.name())) {
+                    sign = ">";
+                }
+                else if (lessThenSet.contains(field.name())) {
+                    sign = "<";
+                }
+
+                return "`" + field.name() + "`" + sign + "?";
+            }).collect(Collectors.toSet())));
         }
 
         if (limit >= 0) {
